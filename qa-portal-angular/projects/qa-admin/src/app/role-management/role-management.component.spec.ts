75d697debd6ee0eef9a56937af549547b471cb6c
ioawnen <ioawnen@gmail.com>
2019-12-05 11:22:22 +0000

    Role management test fixes

--- qa-portal-angular/projects/qa-admin/src/app/role-management/role-management.component.spec.ts
+++ qa-portal-angular/projects/qa-admin/src/app/role-management/role-management.component.spec.ts
@@ -1,16 +1,41 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RoleManagementComponent } from './role-management.component';
+import { MockComponent, MockComponents } from 'ng-mocks';
+import { SpinnerOverlayComponent } from 'projects/qa-common/src/app/spinner-overlay/spinner-overlay.component';
+import { DataTableComponent } from 'projects/qa-common/src/app/data-table/data-table.component';
+import { MatIconModule, MatDialogRef, MatDialogModule } from '@angular/material';
+import { RoleService } from '../_common/services/role.service';
+import { MockRoleService } from '../_mocks/role.service.mock';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MockMatDialogRef } from 'projects/portal-core/src/app/_mocks/mat-dialog-ref.mock';
+import { QaErrorHandlerService } from 'projects/portal-core/src/app/_common/services/qa-error-handler.service';
+import { MockQaErrorHandlerService } from 'projects/portal-core/src/app/_mocks/qa-error-handler.service.mock';
 
-describe('RoleManagementComponent', () => {
+fdescribe('RoleManagementComponent', () => {
   let component: RoleManagementComponent;
   let fixture: ComponentFixture<RoleManagementComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RoleManagementComponent ]
+      declarations: [
+        RoleManagementComponent,
+        MockComponents(
+          SpinnerOverlayComponent,
+          DataTableComponent,
+        )
+      ],
+      imports: [
+        RouterTestingModule,
+        MatIconModule,
+        MatDialogModule
+      ],
+      providers: [
+        { provide: RoleService, useClass: MockRoleService },
+        { provide: QaErrorHandlerService, useClass: MockQaErrorHandlerService }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
