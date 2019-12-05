dbe331e05e2076f6e15c8efb709d635b71a4cb3b
ioawnen <ioawnen@gmail.com>
2019-12-05 10:20:04 +0000

    Update qa-forms tests

--- qa-portal-angular/projects/qa-forms/src/app/form/form.component.spec.ts
+++ qa-portal-angular/projects/qa-forms/src/app/form/form.component.spec.ts
@@ -1,21 +1,36 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FormComponent } from './form.component';
+import { MockComponent } from 'ng-mocks';
+import { CategoryComponent } from '../category/category.component';
+import { IFormModel } from '../_common/models';
 
 describe('FormComponent', () => {
   let component: FormComponent;
   let fixture: ComponentFixture<FormComponent>;
 
+  const mockFormModel: IFormModel = {
+    categoryResponses: [],
+    status: 'SAVED',
+    id: 123,
+    cohortCourse: null,
+    trainee: null
+  };
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FormComponent ]
+      declarations: [
+        FormComponent,
+        MockComponent(CategoryComponent)
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(FormComponent);
     component = fixture.componentInstance;
+    component.model = mockFormModel;
     fixture.detectChanges();
   });
 
