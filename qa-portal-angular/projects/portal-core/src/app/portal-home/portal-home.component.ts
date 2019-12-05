7fbfdeedf3b8885bc9ea9728d38fbb9c32b30b98
renJoseph <ahrennatjoseph@googlemail.com>
2019-12-05 16:07:18 +0000

    First commit

--- qa-portal-angular/projects/portal-core/src/app/portal-home/portal-home.component.ts
+++ qa-portal-angular/projects/portal-core/src/app/portal-home/portal-home.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { ApplicationService } from '../_common/services/application.service';
+import { SideMenuService } from '../_common/services/side-menu.service';
 
 @Component({
   selector: 'app-portal-home',
   templateUrl: './portal-home.component.html',
-  styleUrls: ['./portal-home.component.css']
+  styleUrls: ['./portal-home.component.scss']
 })
 export class PortalHomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public sideMenuService: SideMenuService,
+    public appService: ApplicationService
+  ) { }
 
   ngOnInit() {
   }
