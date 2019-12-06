e5f01803e1d4ea3fb02e1aa158d30fdd6a9f6b52
ioawnen <ioawnen@gmail.com>
2019-12-06 14:40:43 +0000

    Updates admin service, misc component specs

--- qa-portal-angular/projects/qa-admin/src/app/_common/services/cohort.service.spec.ts
+++ qa-portal-angular/projects/qa-admin/src/app/_common/services/cohort.service.spec.ts
@@ -1,9 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { CohortService } from './cohort.service';
+import { QaHttpService } from 'projects/portal-core/src/app/_common/services/qa-http.service';
+import { MockQaHttpService } from 'projects/portal-core/src/app/_mocks/qa-http.service.mock';
 
 describe('CohortService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [
+      { provide: QaHttpService, useClass: MockQaHttpService }
+    ]
+  }));
 
   it('should be created', () => {
     const service: CohortService = TestBed.get(CohortService);
