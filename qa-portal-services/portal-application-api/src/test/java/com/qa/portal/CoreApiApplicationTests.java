b041b302ee854719e8ec48dec141ceaa297c21ef
DreamcastKnight <56401347+DreamcastKnight@users.noreply.github.com>
2019-12-09 10:23:19 +0000

    Update CoreApiApplicationTests.java
    smoke test works

--- qa-portal-services/portal-application-api/src/test/java/com/qa/portal/CoreApiApplicationTests.java
+++ qa-portal-services/portal-application-api/src/test/java/com/qa/portal/CoreApiApplicationTests.java
@@ -1,12 +1,10 @@
 package com.qa.portal;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 public class CoreApiApplicationTests {
-
+	
+	@Test
     public void contextLoads() {
     }
 
