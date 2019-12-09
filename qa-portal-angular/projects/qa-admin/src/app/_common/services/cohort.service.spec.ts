987bcdb4d6c345455983ca6e36ccb6af47122153
ioawnen <ioawnen@gmail.com>
2019-12-09 10:40:13 +0000

    Updates admin dialog tests

--- qa-portal-angular/projects/qa-admin/src/app/_common/services/cohort.service.spec.ts
+++ qa-portal-angular/projects/qa-admin/src/app/_common/services/cohort.service.spec.ts
@@ -3,9 +3,13 @@ import { TestBed } from '@angular/core/testing';
 import { CohortService } from './cohort.service';
 import { QaHttpService } from 'projects/portal-core/src/app/_common/services/qa-http.service';
 import { MockQaHttpService } from 'projects/portal-core/src/app/_mocks/qa-http.service.mock';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('CohortService', () => {
   beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      HttpClientTestingModule
+    ],
     providers: [
       { provide: QaHttpService, useClass: MockQaHttpService }
     ]
