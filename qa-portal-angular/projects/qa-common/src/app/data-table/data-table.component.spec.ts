5cda8a344b2a041400f4b56cc763f3f706b6e459
AfeezSays <56401534+AfeezSays@users.noreply.github.com>
2019-12-09 16:16:47 +0000

    finished rowClick emit test

--- qa-portal-angular/projects/qa-common/src/app/data-table/data-table.component.spec.ts
+++ qa-portal-angular/projects/qa-common/src/app/data-table/data-table.component.spec.ts
@@ -4,8 +4,9 @@ import { DataTableComponent } from './data-table.component';
 import { MatProgressBarModule, MatTableModule, MatCheckboxModule, MatCardModule } from '@angular/material';
 import { CdkTableModule } from '@angular/cdk/table';
 import { DragDropModule } from '@angular/cdk/drag-drop';
+import { doesNotThrow } from 'assert';
 
-describe('DataTableComponent', () => {
+fdescribe('DataTableComponent', () => {
   let component: DataTableComponent<any>;
   let fixture: ComponentFixture<DataTableComponent<any>>;
 
@@ -32,5 +33,35 @@ describe('DataTableComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit a rowClick event when called', done => {
+    const testIndex = 1;
+    const testData = 'test data';
+    const testEvent = new MouseEvent('click');
+    
+    component.rowClick.subscribe(data => {
+      expect(data.index).toEqual(testIndex);
+      expect(data.data).toEqual(testData);
+      expect(data.event).toEqual(testEvent);
+      
+      done();
+    });
+
+    component.onRowClicked(testIndex, testData, testEvent);
+  });
+
+  it('should update when a row is selected', () => {
+    
+  });
+
+  it('should deselect all rows', done => {
+    component.rowSelectionChange.subscribe(data => {
+      expect(data).toEqual(component.rowSelection);
+
+      done();
+    });
+
+    component.deselectAllRows();
   });
 });
