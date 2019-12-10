125cf433641ec2ae2190786e4458aea9778a0628
Kyle <xkylemcx13@gmail.com>
2019-12-10 12:02:30 +0000

    completed 100% testing in course-feedback evaluation-table

--- qa-portal-angular/projects/course-feedback/src/app/evaluation-table/evaluation-table.component.spec.ts
+++ qa-portal-angular/projects/course-feedback/src/app/evaluation-table/evaluation-table.component.spec.ts
@@ -3,7 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { EvaluationTableComponent } from './evaluation-table.component';
 import { MatTableModule } from '@angular/material';
 
-fdescribe('EvaluationTableComponent', () => {
+describe('EvaluationTableComponent', () => {
   let component: EvaluationTableComponent;
   let fixture: ComponentFixture<EvaluationTableComponent>;
 
