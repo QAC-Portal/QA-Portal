cb1acbe37b1dad4d877f7cdceab339d70af07f80
Kyle <xkylemcx13@gmail.com>
2019-12-10 12:01:11 +0000

    completed 100% testing in course-feedback evaluation-table

--- qa-portal-angular/projects/course-feedback/src/app/evaluation-table/evaluation-table.component.spec.ts
+++ qa-portal-angular/projects/course-feedback/src/app/evaluation-table/evaluation-table.component.spec.ts
@@ -1,14 +1,18 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EvaluationTableComponent } from './evaluation-table.component';
+import { MatTableModule } from '@angular/material';
 
-describe('EvaluationTableComponent', () => {
+fdescribe('EvaluationTableComponent', () => {
   let component: EvaluationTableComponent;
   let fixture: ComponentFixture<EvaluationTableComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ EvaluationTableComponent ]
+      declarations: [ EvaluationTableComponent ],
+      imports: [
+        MatTableModule
+      ]
     })
     .compileComponents();
   }));
@@ -22,4 +26,9 @@ describe('EvaluationTableComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('sanity checks', () => {
+    expect(component.displayedColumns).toEqual(['course', 'start', 'end', 'courseid', 'pin', 'status']);
+  });
+
 });
