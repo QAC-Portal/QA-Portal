c690e46836a8ce137692f230409da61f155ca65f
renJoseph <ahrennatjoseph@googlemail.com>
2019-12-11 12:21:39 +0000

    Removing conflict code

--- qa-portal-angular/projects/portal-core/src/app/_common/services/qa-http.service.spec.ts
+++ qa-portal-angular/projects/portal-core/src/app/_common/services/qa-http.service.spec.ts
@@ -1,29 +1,12 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 
 import { QaHttpService } from './qa-http.service';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
-fdescribe('QaHttpService', () => {
+describe('QaHttpService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
 
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [
-      HttpClientTestingModule
-    ]
-  }));
-
-
-  it('should be created', inject([HttpTestingController, QaHttpService], (httpMock: HttpTestingController, service: QaHttpService) => {
+  it('should be created', () => {
+    const service: QaHttpService = TestBed.get(QaHttpService);
     expect(service).toBeTruthy();
-  }));
-  it('should send get request using ref', inject([HttpTestingController, QaHttpService], (httpMock: HttpTestingController, service: QaHttpService) => {
-    const mockGetReq = httpMock.expectOne('/test/url');
-
-    service.get('/test/url').subscribe(body => {
-      expect(body).toEqual('test body');
-    });
-
-    expect(mockGetReq.request.body).toEqual('incoming body');
-    mockGetReq.flush('test body');
-    httpMock.verify();
-  }));
+  });
 });
