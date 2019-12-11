cda0b4ac986fece7af4e66312fcb301af67b4f8a
BackRowOla <Babalakin.Oyeyemi@academytrainee.com>
2019-12-11 12:53:09 +0000

    reached 100% coverage for trainer evaluation history service

--- qa-portal-angular/projects/course-feedback/src/app/trainer-evaluation-history/services/retrieve-trainer-evaluation-history.service.spec.ts
+++ qa-portal-angular/projects/course-feedback/src/app/trainer-evaluation-history/services/retrieve-trainer-evaluation-history.service.spec.ts
@@ -1,12 +1,41 @@
 import { TestBed } from '@angular/core/testing';
-
 import { RetrieveTrainerEvaluationHistoryService } from './retrieve-trainer-evaluation-history.service';
+import { HttpClient } from '@angular/common/http';
+import { of } from 'rxjs';
+import { GET_COHORT_COURSES_FOR_TRAINER } from '../../_common/models/course-feedback.constants';
 
-describe('RetrieveTrainerEvaluationHistoryService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+class MockHttpClient {
+  public get() {
+    return of(null);
+  }
+}
+
+fdescribe('RetrieveTrainerEvaluationHistoryService', () => {
+  let service: RetrieveTrainerEvaluationHistoryService;
+  let httpClient: HttpClient;
+
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [
+      RetrieveTrainerEvaluationHistoryService,
+      { provide: HttpClient, useClass: MockHttpClient }
+    ]
+  }));
+
+  beforeEach(() => {
+    service = TestBed.get(RetrieveTrainerEvaluationHistoryService);
+    httpClient = TestBed.get(HttpClient);
+  });
 
   it('should be created', () => {
-    const service: RetrieveTrainerEvaluationHistoryService = TestBed.get(RetrieveTrainerEvaluationHistoryService);
     expect(service).toBeTruthy();
   });
+
+  it('should reach the correct endpoint url', done => {
+    httpClient.get = (url) => {
+      expect(url).toEqual(GET_COHORT_COURSES_FOR_TRAINER);
+      return of(null);
+    };
+    service.getEvalHistory().subscribe(() => done());
+  });
+
 });
