cba456267aec8a1bc9c48ee5b659d0eeacb12798
Hayden Lamb <haydenlamb97@gmail.com>
2019-12-12 14:15:00 +0000

    k

--- qa-portal-angular/projects/qa-cv/src/app/cv-card-generate/cv-card-generate.component.spec.ts
+++ qa-portal-angular/projects/qa-cv/src/app/cv-card-generate/cv-card-generate.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { CvCardBaseComponent } from './cv-card-base.component';
+import { CvCardBaseComponent } from './cv-card-generate.component';
 
 describe('CvCardBaseComponent', () => {
   let component: CvCardBaseComponent;
