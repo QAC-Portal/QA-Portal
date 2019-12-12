47aca763a41160f4dc0da7924ce1a9c7ab2631b2
andrewjackson95 <andrewjackson95@hotmail.co.uk>
2019-12-12 12:11:17 +0000

    Increased side-menu.service test coverage to 100%

--- qa-portal-angular/projects/portal-core/src/app/_common/services/side-menu.service.spec.ts
+++ qa-portal-angular/projects/portal-core/src/app/_common/services/side-menu.service.spec.ts
@@ -3,14 +3,40 @@ import { TestBed } from '@angular/core/testing';
 import { SideMenuService } from './side-menu.service';
 
 describe('SideMenuService', () => {
+   
+  let service: SideMenuService;
+
   beforeEach(() => TestBed.configureTestingModule({}));
 
+  beforeEach(() => {
+    localStorage.clear();
+    service = TestBed.get(SideMenuService);
+    
+  })
+
   it('should be created', () => {
-    const service: SideMenuService = TestBed.get(SideMenuService);
+
     expect(service).toBeTruthy();
   });
 
-  it('should open the side menu', () => {
+  it('should call toggleOpen', () => {
+    spyOn(service, 'toggleOpen').and.callThrough();
+    service.toggleOpen();
+
+    expect(service.toggleOpen).toHaveBeenCalledTimes(1);
+  });
+
+  it('should set sideMenuOpen to true on init', () => {
     
+    
+    
+    expect(service.sideMenuOpen).toEqual(false);
+   //expect(localStorage.getItem('sideMenuOpen')).toEqual('true');
+  });
+  it('should open side menu', () => {
+    service.toggleOpen();
+    
+    
+    expect(localStorage.getItem('sideMenuOpen')).toEqual('true');
   });
 });
