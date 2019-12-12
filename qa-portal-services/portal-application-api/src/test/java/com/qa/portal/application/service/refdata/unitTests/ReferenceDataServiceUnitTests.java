0c6855ac1213cbfef6a2e1974e1d64423dc8eacc
Thepworth95 <Thomas.hepworth@academytrainee.com>
2019-12-12 16:42:45 +0000

    Compleated Portal application Rest unit tests

---
+++ qa-portal-services/portal-application-api/src/test/java/com/qa/portal/application/service/refdata/unitTests/ReferenceDataServiceUnitTests.java
@@ -0,0 +1,35 @@
+package com.qa.portal.application.service.refdata.unitTests;
+
+//import static org.assertj.core.api.Assertions.assertThat;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.mockito.InjectMocks;
+//import org.mockito.Mock;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//import com.qa.portal.application.service.refdata.ReferenceDataService;
+//import com.qa.portal.common.persistence.refdata.QaReferenceData;
+
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+public class ReferenceDataServiceUnitTests {
+//	
+//	@InjectMocks
+//	ReferenceDataService referenceDataService;
+//	
+//	@Test
+//	public void getReferenceDataForCategoriesTest() {
+//		List<QaReferenceData> list = new ArrayList<QaReferenceData>();
+//		
+//		ReferenceDataService referenceDataServiceElement = new ReferenceDataService(list);
+//		
+//		
+//		assertThat(this.referenceDataService.getReferenceDataForCategories()).isEqualTo(referenceDataService);
+//	}
+
+}
