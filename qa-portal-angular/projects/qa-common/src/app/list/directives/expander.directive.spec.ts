32f92deaee90d339f40c4fd461c33be1e7da9e08
Kyle <xkylemcx13@gmail.com>
2019-12-12 10:48:22 +0000

    completed 100% testing in common

--- qa-portal-angular/projects/qa-common/src/app/list/directives/expander.directive.spec.ts
+++ qa-portal-angular/projects/qa-common/src/app/list/directives/expander.directive.spec.ts
@@ -1,8 +1,19 @@
 import { ExpanderDirective } from './expander.directive';
+import { ViewContainerRef } from '@angular/core';
 
 describe('ExpanderDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ExpanderDirective(null, null);
+  let directive: ExpanderDirective;
+  beforeEach(() => {
+    const vcr = { createEmbeddedView: (a, b) => { } } as ViewContainerRef;
+    directive = new ExpanderDirective(null, vcr);
+  })
+  it('should create', () => {
     expect(directive).toBeTruthy();
+  });
+
+  it('should call creatEmbeddedView on init', () => {
+    spyOn((directive as any).viewContainer, 'createEmbeddedView').and.callThrough();
+    directive.ngOnInit();
+    expect((directive as any).viewContainer.createEmbeddedView).toHaveBeenCalledTimes(1);
   });
 });
