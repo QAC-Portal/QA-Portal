cacd6519bb5491ee798ef40c1d4023743a62a90e
Dan Ryan <dryan.dev@gmail.com>
2019-12-13 10:16:14 +0000

    started REST testing

--- qa-portal-services/cv-api/src/test/java/com/qa/portal/cv/rest/CvManagementControllerTest.java
+++ qa-portal-services/cv-api/src/test/java/com/qa/portal/cv/rest/CvManagementControllerTest.java
@@ -1,13 +1,20 @@
 package com.qa.portal.cv.rest;
 
+import com.qa.portal.common.security.QaSecurityContext;
+import com.qa.portal.cv.domain.CvVersion;
+import com.qa.portal.cv.services.CvManagementService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -16,9 +23,25 @@ public class CvManagementControllerTest {
     @InjectMocks
     public CvManagementController cvManagementController;
 
-    @Before
-    public void setVariables() {
+    @Mock
+    public CvManagementService cvManagementService;
+    public QaSecurityContext qaSecurityContext;
+
+    @Test
+    public void createCvTest() {
+        CvVersion cvVersion = new CvVersion();
+        Mockito.when(this.cvManagementService.createCv(cvVersion, qaSecurityContext)).thenReturn(cvVersion);
+        assertEquals(this.cvManagementController.createCv(cvVersion).getBody(), cvVersion);
     }
 
+    @Test
+    public void updateCvTest() {
+        CvVersion cvVersion = new CvVersion();
+        Mockito.when(this.cvManagementService.updateCv(cvVersion)).thenReturn(cvVersion);
+        assertEquals(this.cvManagementController.updateCv(cvVersion).getBody(), cvVersion);
+    }
+
+//    @Test
+//    public void
 
 }
