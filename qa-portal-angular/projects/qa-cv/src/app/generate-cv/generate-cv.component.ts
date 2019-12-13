6ae69620764ce6d773e43ba43e4f8f98dcf81088
Malverick <jamesfirth95@yahoo.co.uk>
2019-12-13 11:42:44 +0000

    Added a space between the bottom of hobbies, and the button bar

--- qa-portal-angular/projects/qa-cv/src/app/generate-cv/generate-cv.component.ts
+++ qa-portal-angular/projects/qa-cv/src/app/generate-cv/generate-cv.component.ts
@@ -11,7 +11,7 @@ import { IN_PROGRESS_STATUS, FAILED_REVIEW_STATUS, APPROVED_STATUS, FOR_REVIEW_S
 import { Observable } from 'rxjs';
 import { QaErrorHandlerService } from 'projects/portal-core/src/app/_common/services/qa-error-handler.service';
 import { ViewCvStateManagerService } from '../view-cv/services/view-cv-state-manager.service';
-import { ActivatedRoute, ParamMap } from '@angular/router';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import { CvCardBaseComponent } from '../cv-card-base/cv-card-base.component';
 import { ADMIN_CV_SEARCH_URL } from '../_common/models/cv.constants';
 
@@ -65,7 +65,8 @@ export class GenerateCvComponent implements OnInit {
     private viewCvStateManagerService: ViewCvStateManagerService,
     private VCvService: ViewCvService,
     private cvService: CvService,
-    private errorHandlerService: QaErrorHandlerService
+    private errorHandlerService: QaErrorHandlerService,
+    private router: Router
   ) {
     const fb = new FormBuilder();
     this.cvForm = fb.group({
