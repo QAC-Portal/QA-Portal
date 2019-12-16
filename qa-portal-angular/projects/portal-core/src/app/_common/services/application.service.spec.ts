7f70668537c5f0a6c809a822862d96711a45009e
EkowTheGreat <e-asante@hotmail.com>
2019-12-16 09:59:46 +0000

    incomplete testing for application service

--- qa-portal-angular/projects/portal-core/src/app/_common/services/application.service.spec.ts
+++ qa-portal-angular/projects/portal-core/src/app/_common/services/application.service.spec.ts
@@ -1,12 +1,54 @@
 import { TestBed } from '@angular/core/testing';
-
+import { MockApplicationService } from '../../_mocks/application.service.mock';
+import { QaHttpService } from './qa-http.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ApplicationService } from './application.service';
+import { NavigationEnd } from '@angular/router';
+import { PortalApplicationProjectsModel } from '../models/portal-application-projects.model';
+import { MockQaHttpService } from '../../_mocks/qa-http.service.mock';
 
 describe('ApplicationService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+
+  let service: ApplicationService;
+ 
+  let navEnd: NavigationEnd;
+ 
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule, HttpClientTestingModule
+      ],
+      providers: [
+        ApplicationService, //NavigationEnd,
+        //{provide: ApplicationService, useClass: MockApplicationService},
+        {provide: QaHttpService, useClass: MockQaHttpService}  
+      
+      ]
+    });
+  });
+
+  beforeEach(() => {
+    service = TestBed.get(ApplicationService);
+    
+    
+    //navEnd  = TestBed.get(NavigationEnd);
+    
+  });
+
 
   it('should be created', () => {
-    const service: ApplicationService = TestBed.get(ApplicationService);
     expect(service).toBeTruthy();
   });
+  it('should call onApplicationLoaded once', () => {
+    spyOn(service, 'onApplicationLoaded').and.callThrough();
+    //navEnd.urlAfterRedirects = 'cohort-api/manage/user';
+    //navEnd = new NavigationEnd(1,'cohort-api/manage/user',navEnd.urlAfterRedirects);
+    service.onApplicationLoaded();
+    
+    expect(service.onApplicationLoaded).toHaveBeenCalledTimes(1);
+
+  });
+ 
 });
