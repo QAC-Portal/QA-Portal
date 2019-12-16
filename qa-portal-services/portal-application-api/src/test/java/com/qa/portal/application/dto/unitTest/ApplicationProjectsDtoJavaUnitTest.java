49a677e933c3644ccefec14d8bbc176ab494ac81
z9170qa <zak.meyers@academytrainee.com>
2019-12-16 16:16:41 +0000

    ApplicationProjectsDto tests cresated, 100% coverage for file

--- qa-portal-services/portal-application-api/src/test/java/com/qa/portal/application/dto/unitTest/ApplicationProjectsDtoJavaUnitTest.java
+++ qa-portal-services/portal-application-api/src/test/java/com/qa/portal/application/dto/unitTest/ApplicationProjectsDtoJavaUnitTest.java
@@ -28,9 +28,47 @@ public class ApplicationProjectsDtoJavaUnitTest {
 	@Mock
 	ApplicationProjectsDto applicationProjectsDtoNull = null;
 	String applicationProjectsDtoDiffClass = "this is a string";
-	ApplicationProjectsDto applicationProjectsDtoDiffPortalApplicationDto = new ApplicationProjectsDto(null, null);
+	ApplicationProjectsDto applicationProjectsDtoDiffPortalApplication = new ApplicationProjectsDto(null, null);
 	ApplicationProjectsDto applicationProjectsDtoDiffPortalProjects = new ApplicationProjectsDto(null, null);
 	
+	@Before
+	public void setup() {
+		PortalApplicationDto portalApplicationDto = new PortalApplicationDto();
+		portalApplicationDto.setId(1);
+		this.applicationProjectsDto.setPortalApplication(portalApplicationDto);
+		PortalProjectDto portalProjectDto1 = new PortalProjectDto();
+		PortalProjectDto portalProjectDto2 = new PortalProjectDto();
+		portalProjectDto1.setId(1);
+		portalProjectDto2.setId(2);
+		Set<PortalProjectDto> portalProjectSet = new HashSet<PortalProjectDto>();
+		portalProjectSet.add(portalProjectDto1);
+		portalProjectSet.add(portalProjectDto2);
+		this.applicationProjectsDto.setPortalProjects(portalProjectSet);
+		
+		PortalApplicationDto portalApplicationDtoDiffPortalApplication = new PortalApplicationDto();
+		portalApplicationDtoDiffPortalApplication.setId(2);
+		this.applicationProjectsDtoDiffPortalApplication.setPortalApplication(portalApplicationDtoDiffPortalApplication);
+		PortalProjectDto portalProjectDtoDiffPortalApplication1 = new PortalProjectDto();
+		PortalProjectDto portalProjectDtoDiffPortalApplication2 = new PortalProjectDto();
+		portalProjectDtoDiffPortalApplication1.setId(1);
+		portalProjectDtoDiffPortalApplication2.setId(2);
+		Set<PortalProjectDto> portalProjectSetDiffPortalApplication = new HashSet<PortalProjectDto>();
+		portalProjectSetDiffPortalApplication.add(portalProjectDtoDiffPortalApplication1);
+		portalProjectSetDiffPortalApplication.add(portalProjectDtoDiffPortalApplication2);
+		this.applicationProjectsDtoDiffPortalApplication.setPortalProjects(portalProjectSetDiffPortalApplication);
+		
+		PortalApplicationDto portalApplicationDtoDiffPortalProjects = new PortalApplicationDto();
+		portalApplicationDtoDiffPortalProjects.setId(1);
+		this.applicationProjectsDtoDiffPortalProjects.setPortalApplication(portalApplicationDtoDiffPortalProjects);
+		PortalProjectDto portalProjectDtoDiffPortalProjects1 = new PortalProjectDto();
+		PortalProjectDto portalProjectDtoDiffPortalProjects2 = new PortalProjectDto();
+		portalProjectDtoDiffPortalProjects1.setId(3);
+		portalProjectDtoDiffPortalProjects2.setId(4);
+		Set<PortalProjectDto> portalProjectSetDiffPortalProjects = new HashSet<PortalProjectDto>();
+		portalProjectSetDiffPortalProjects.add(portalProjectDtoDiffPortalProjects1);
+		portalProjectSetDiffPortalProjects.add(portalProjectDtoDiffPortalProjects2);
+		this.applicationProjectsDtoDiffPortalProjects.setPortalProjects(portalProjectSetDiffPortalProjects);
+	}
 	
 		
 	
@@ -38,43 +76,30 @@ public class ApplicationProjectsDtoJavaUnitTest {
 	
 	@Test
 	public void getPortalApplicationTest() {
-		PortalApplicationDto portalApplicationDto = new PortalApplicationDto();
-		portalApplicationDto.setId(1);
-		this.applicationProjectsDto.setPortalApplication(portalApplicationDto);
-		PortalProjectDto portalProjectDto1 = new PortalProjectDto();
-		PortalProjectDto portalProjectDto2 = new PortalProjectDto();
-		portalProjectDto1.setId(1);
-		portalProjectDto2.setId(2);
-		Set<PortalProjectDto> portalProjectSet = new HashSet<PortalProjectDto>();
-		portalProjectSet.add(portalProjectDto1);
-		portalProjectSet.add(portalProjectDto2);
-		this.applicationProjectsDto.setPortalProjects(portalProjectSet);
 		assertThat(String.valueOf(this.applicationProjectsDto.getPortalApplication())).isEqualTo("PortalApplicationDto{id=1, name=\'null\', description=\'null\', displayOrder=null, baseUrl=\'null\'}"); 
 	}
 	
 	@Test
 	public void getPortalProjectsTest() {
-		PortalApplicationDto portalApplicationDto = new PortalApplicationDto();
-		portalApplicationDto.setId(1);
-		this.applicationProjectsDto.setPortalApplication(portalApplicationDto);
-		PortalProjectDto portalProjectDto1 = new PortalProjectDto();
-		PortalProjectDto portalProjectDto2 = new PortalProjectDto();
-		portalProjectDto1.setId(1);
-		portalProjectDto2.setId(2);
-		Set<PortalProjectDto> portalProjectSet = new HashSet<PortalProjectDto>();
-		portalProjectSet.add(portalProjectDto1);
-		portalProjectSet.add(portalProjectDto2);
-		this.applicationProjectsDto.setPortalProjects(portalProjectSet);
-		assertThat(String.valueOf(this.applicationProjectsDto.getPortalProjects())).isEqualTo(String.valueOf((portalProjectSet))); 
+		assertThat(String.valueOf(this.applicationProjectsDto.getPortalProjects())).isEqualTo("[com.qa.portal.application.dto.PortalProjectDto@f003f, com.qa.portal.application.dto.PortalProjectDto@e8be0]"); 
 	}
 	
 	@Test
 	public void hashTest() {
-		assertThat(String.valueOf(this.applicationProjectsDto.hashCode()).length()).isEqualTo(3);
+		assertThat(String.valueOf(this.applicationProjectsDto.hashCode()).length()).isEqualTo(9);
+	}
+	
+	@Test
+	public void equalsTest() {
+		assertThat(this.applicationProjectsDto.equals(applicationProjectsDto)).isEqualTo(true);
+		assertThat(this.applicationProjectsDto.equals(applicationProjectsDtoNull)).isEqualTo(false);
+		assertThat(this.applicationProjectsDto.equals(applicationProjectsDtoDiffClass)).isEqualTo(false);
+		assertThat(this.applicationProjectsDto.equals(applicationProjectsDtoDiffPortalApplication)).isEqualTo(false);
+		assertThat(this.applicationProjectsDto.equals(applicationProjectsDtoDiffPortalProjects)).isEqualTo(false);
 	}
 	
 	@Test
 	public void toStringTest() {
-		assertThat((this.applicationProjectsDto.toString())).isEqualTo("DepartmentApplicationsDto{department=null, applications=null}");
+		assertThat((this.applicationProjectsDto.toString())).isEqualTo("DepartmentApplicationsDto{department=PortalApplicationDto{id=1, name=\'null\', description=\'null\', displayOrder=null, baseUrl=\'null\'}, applications=[com.qa.portal.application.dto.PortalProjectDto@f003f, com.qa.portal.application.dto.PortalProjectDto@e8be0]}");
 	}
 }
