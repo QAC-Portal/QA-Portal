fa7682b543993729777b3b8f958f3a3f93b51aa6
DreamcastKnight <56401347+DreamcastKnight@users.noreply.github.com>
2019-12-17 11:15:12 +0000

    done small amount fo testing in service.mapper

--- qa-portal-services/portal-application-api/src/test/java/com/qa/portal/application/service/page/unitTest/ProjectPageMapperTest.java
+++ qa-portal-services/portal-application-api/src/test/java/com/qa/portal/application/service/page/unitTest/ProjectPageMapperTest.java
@@ -1,8 +1,6 @@
 package com.qa.portal.application.service.page.unitTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.hamcrest.CoreMatchers.containsString;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -91,52 +89,4 @@ public class ProjectPageMapperTest {
 		assertThat(projectPageMapper.mapToProjectPageDto(projectPageEntity)).isEqualTo(pageDto);
 	}
 	
-	@Test
-	public void mapToNewProjectPageEntityTest() {
-		ProjectPageDto pageDto = new ProjectPageDto();
-		pageDto.setDisplayOnMenu(true);
-		pageDto.setIcon("icon");
-		pageDto.setId(1);
-		pageDto.setLevel(1);
-		pageDto.setName("pname");
-		pageDto.setPortalProjectName("p-p-name");
-		List<String> dummyData = new ArrayList<String>();
-		dummyData.add("a");
-		dummyData.add("b");
-		dummyData.add("c");
-		pageDto.setRoles(dummyData);
-		pageDto.setTooltip("tooltip");
-		pageDto.setUrl("www.test.co.uk");
-
-		ProjectPageEntity projectPageEntity2 = new ProjectPageEntity();
-
-		PortalProjectEntity portalProjectEntity = new PortalProjectEntity();
-		portalProjectEntity.setId(1);
-		portalProjectEntity.setName("ppname");
-		List<ProjectPageEntity> projectPageList = new ArrayList<ProjectPageEntity>();
-		projectPageList.add(projectPageEntity2);
-		portalProjectEntity.setProjectPages(projectPageList);
-
-		RoleEntity roleEntity = new RoleEntity();
-		roleEntity.setName("test name");
-
-		RoleProjectPageEntity roleProjectPageEntity = new RoleProjectPageEntity();
-		roleProjectPageEntity.setId(1);
-		roleProjectPageEntity.setRole(roleEntity);
-
-		ProjectPageEntity projectPageEntity = new ProjectPageEntity();
-		projectPageEntity.setId(1);
-		projectPageEntity.setName("pename");
-		projectPageEntity.setUrl("www.url.com");
-		projectPageEntity.setTooltip("tooltip two");
-		projectPageEntity.setIcon("icons");
-		projectPageEntity.setPortalProject(portalProjectEntity);
-		List<RoleProjectPageEntity> roleProjectPageEntityList = new ArrayList<RoleProjectPageEntity>();
-		roleProjectPageEntityList.add(roleProjectPageEntity);
-		projectPageEntity.setRoleProjectPageEntities(roleProjectPageEntityList);
-		projectPageEntity.setDisplayOnMenu(true);
-		
-		Mockito.when(baseMapper.mapObject(pageDto, ProjectPageEntity.class)).thenReturn(projectPageEntity);
-		//Mockito.when(roleRepository.findByName());
-	}
-}
+}  
