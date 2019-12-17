589fabd4643e0978cc95feae38cf8f7e08499b9f
Malverick <jamesfirth95@yahoo.co.uk>
2019-12-17 14:15:37 +0000

    added a test

--- qa-portal-services/portal-application-api/src/test/java/com/qa/portal/application/service/page/mapper/ProjectPageMapperTest.java
+++ qa-portal-services/portal-application-api/src/test/java/com/qa/portal/application/service/page/mapper/ProjectPageMapperTest.java
@@ -1,44 +1,72 @@
 package com.qa.portal.application.service.page.mapper;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import java.util.ArrayList;
 import java.util.List;
-
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import com.qa.portal.application.dto.ProjectPageDto;
 import com.qa.portal.application.persistence.entity.PortalProjectEntity;
 import com.qa.portal.application.persistence.entity.ProjectPageEntity;
+import com.qa.portal.application.persistence.entity.RoleEntity;
 import com.qa.portal.application.persistence.entity.RoleProjectPageEntity;
-import com.qa.portal.application.persistence.repository.RoleRepository;
+import com.qa.portal.application.persistence.repository.ProjectPageRepository;
+import com.qa.portal.application.service.page.mapper.ProjectPageMapper;
 import com.qa.portal.common.service.mapper.BaseMapper;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class ProjectPageMapperTest {
+
 	@InjectMocks
 	public ProjectPageMapper projectPageMapper;
 
 	@Mock
-	public BaseMapper baseMapper;
+	public ProjectPageRepository projectPageRepository;
 
 	@Mock
-	public RoleRepository roleRepository;
+	public BaseMapper baseMapper;
 
 	@Test
-	public void mapToProjectPageDtoTest() {
+	public void mapToProjectPageTest() {
+		ProjectPageDto pageDto = new ProjectPageDto();
+		pageDto.setDisplayOnMenu(true);
+		pageDto.setIcon("icon");
+		pageDto.setId(1);
+		pageDto.setLevel(1);
+		pageDto.setName("pname");
+		pageDto.setPortalProjectName("p-p-name");
+		List<String> dummyData = new ArrayList<String>();
+		dummyData.add("a");
+		dummyData.add("b");
+		dummyData.add("c");
+		pageDto.setRoles(dummyData);
+		pageDto.setTooltip("tooltip");
+		pageDto.setUrl("www.test.co.uk");
+
 		ProjectPageEntity projectPageEntity2 = new ProjectPageEntity();
 
 		PortalProjectEntity portalProjectEntity = new PortalProjectEntity();
-
 		portalProjectEntity.setId(1);
 		portalProjectEntity.setName("ppname");
-		List<ProjectPageEntity> projectPageEntityList = new ArrayList<ProjectPageEntity>();
-		projectPageEntityList.add(projectPageEntity2);
-		portalProjectEntity.setProjectPages(projectPageEntityList);
+		List<ProjectPageEntity> projectPageList = new ArrayList<ProjectPageEntity>();
+		projectPageList.add(projectPageEntity2);
+		portalProjectEntity.setProjectPages(projectPageList);
+
+		RoleEntity roleEntity = new RoleEntity();
+		roleEntity.setName("test name");
 
 		RoleProjectPageEntity roleProjectPageEntity = new RoleProjectPageEntity();
+		roleProjectPageEntity.setId(1);
+		roleProjectPageEntity.setRole(roleEntity);
 
 		ProjectPageEntity projectPageEntity = new ProjectPageEntity();
-
 		projectPageEntity.setId(1);
 		projectPageEntity.setName("pename");
 		projectPageEntity.setUrl("www.url.com");
@@ -49,6 +77,9 @@ public class ProjectPageMapperTest {
 		roleProjectPageEntityList.add(roleProjectPageEntity);
 		projectPageEntity.setRoleProjectPageEntities(roleProjectPageEntityList);
 		projectPageEntity.setDisplayOnMenu(true);
+		
 
+		Mockito.when(baseMapper.mapObject(projectPageEntity, ProjectPageDto.class)).thenReturn(pageDto);
+		assertThat(projectPageMapper.mapToProjectPageDto(projectPageEntity)).isEqualTo(pageDto);
 	}
 }
