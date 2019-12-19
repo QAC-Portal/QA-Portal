286c7dac662fabd1037ca12a4036ca1c6778eb7a
renJoseph <ahrennatjoseph@googlemail.com>
2019-12-19 09:54:20 +0000

    update

--- qa-portal-angular/projects/self-reflection/src/app/cohort-summary/cohort-summary.component.ts
+++ qa-portal-angular/projects/self-reflection/src/app/cohort-summary/cohort-summary.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, Input, Optional } from '@angular/core';
 import { SummaryService } from './services/summary.service';
 import { Observable, Subscription } from 'rxjs';
 import { CohortSummaryModel } from '../_common/models/cohort-summary.model';
 import { QaErrorHandlerService } from '../../../../portal-core/src/app/_common/services/qa-error-handler.service';
+
+import * as lodash from 'lodash';
 
 @Component({
   selector: 'app-cohort-summary',
@@ -18,19 +20,38 @@ export class CohortSummaryComponent implements OnInit, OnDestroy {
   cohortSummary$: Observable<CohortSummaryModel[]>;
   cohortsForGraph: CohortSummaryModel[];
 
+  uniqueTrainingPathways = [];
+  uniqueTrainingPathwayVersions = [];
+
   constructor(private summaryService: SummaryService,
     private errorHandler: QaErrorHandlerService) {
   }
+
+  @Input() value: string | string[];
 
   ngOnInit() {
     this.cohortSummary$ = this.summaryService.getSummary();
     this.summarySubscription = this.cohortSummary$.subscribe((summaries) => {
       this.loadingData = false;
+
+      this.uniqueTrainingPathways = lodash.uniq(summaries.map(s => s.pathway));
     },
       (error) => {
         this.loadingData = false;
         this.errorHandler.handleError(error);
       });
+  }
+
+  updatePathwayVersions() {
+    this.cohortSummary$ = this.summaryService.getSummary();
+    this.summarySubscription = this.cohortSummary$.subscribe((summaries) => {
+      if (!this.value) {
+        this.uniqueTrainingPathwayVersions = lodash.uniq(summaries.map(s => s.pathwayVersion));
+      } else {
+        this.uniqueTrainingPathwayVersions = lodash.uniq(summaries.filter(s => s.pathway === this.value)
+          .map(s => s.pathwayVersion));
+      }
+    });
   }
 
   ngOnDestroy() {
